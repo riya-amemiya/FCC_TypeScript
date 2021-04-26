be4e7a242a2c0a7d06eab078f912cc2f46ebbce8
Riya <hiyoriorria@gmail.com>
2021-04-26 23:08:42 +0900

    大幅な仕様の変更

--- src/FCC_modules/index.ts
+++ src/FCC_modules/index.ts
@@ -6,9 +6,6 @@ import * as Maths from './Math';
 //Props
 import * as Props from './Props';
 
-//type
-import * as type from './type/type';
-
 const version: string = '1.0';
 
-export { version, Ajax, Maths, Props, type };
+export { version, Ajax, Maths, Props };
